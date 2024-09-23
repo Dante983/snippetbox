7498bcdb9fc396223b381dd637568f7adb5ec299
Nikola Savic <savicn209@gmail.com>
2024-09-23 23:19:47 +0200

    added tls certificates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -51,6 +51,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		errorLog:       errorLog,
@@ -69,7 +70,7 @@ func main() {
 
 	infoLog.Printf("Starting server on %s", *addr)
 
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
