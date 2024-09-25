a11ed72c6b875efd4fea0ae7fd59c10401cad001
Nikola Savic <savicn209@gmail.com>
2024-09-26 00:12:30 +0200

    added generic function

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -56,8 +56,10 @@ func MaxChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) <= n
 }
 
-// PermittedInt() returns true if a value is in a list of permitted integers.
-func PermittedInt(value int, permittedValues ...int) bool {
+// Replace PermittedInt() with a generic PermittedValue() function. This returns
+// true if the value of type T equals one of the variadic permittedValues
+// parameters.
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
 	for i := range permittedValues {
 		if value == permittedValues[i] {
 			return true
